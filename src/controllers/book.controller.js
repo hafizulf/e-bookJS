83da4433c66ce738925b30ac8d0caf2ac758e0cd
hafizulf <hafizulf68@gmail.com>
2023-02-09 13:38:06 +0700

    update: GET /books

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,12 +1,12 @@
 const bookService = require('../use-cases/book');
 
-const findAll = (req, res) => {
-  bookService.findAll().then((result) => {
-    return res.status(200).json({
-      status: 'OK',
-      code: 200,
-      data: result,
-    });
+const findAll = async (req, res) => {
+  const result = await bookService.findAll();
+
+  return res.status(200).json({
+    status: 'OK',
+    code: 200,
+    data: result,
   });
 };
 
--- test/book/api.spec.js
+++ test/book/api.spec.js
@@ -1,17 +1,9 @@
-/*
- * Some line of codes isn't remove just to remind me to learning about that later
- * How to stub -> controllers/repository
- */
-
 let chai,
   { expect } = require('chai');
-const sinon = require('sinon');
 const request = require('supertest');
 
 const app = require('../../src/frameworks/app');
 const database = require('../../src/frameworks/database/knex');
-const bookController = require('../../src/controllers/book.controller');
-// const { bookRepository } = require('../../src/repositories');
 
 describe('books API', () => {
   const mockData = [
@@ -39,10 +31,6 @@ describe('books API', () => {
     },
   ];
 
-  // beforeEach(() => {
-  //   sCtlFindAllStub = sinon.stub(bookController, 'findAll');
-  // });
-
   describe('seed books', () => {
     it('should insert new books', async () => {
       await database.raw('ALTER TABLE ' + 'books' + ' AUTO_INCREMENT = 1'); // resetting auto increment
@@ -56,40 +44,35 @@ describe('books API', () => {
     });
 
     it('should return list of books', (done) => {
-      const mockResponse = {
+      const response = {
         status: 'OK',
         code: 200,
         data: mockData,
       };
 
-      // sCtlFindAllStub.returns(mockResponse);
-
       request(app)
         .get('/books')
         .end((err, res) => {
           expect(res.status).to.equal(200);
-          expect(res.body).to.deep.equal(mockResponse);
+          expect(res.body).to.deep.equal(response);
           done();
         });
     });
   });
 
   describe('/books -> with empty data', () => {
-    let sCtlFindAllStub;
-
     it("should return empty array if result doesn't exist", (done) => {
-      const mockResponse = {
+      const response = {
         status: 'OK',
         code: 200,
         data: [],
       };
-      // sCtlFindAllStub.resolves(mockResponse);
 
       request(app)
         .get('/books')
         .end((err, res) => {
           expect(res.status).to.equal(200);
-          expect(res.body).to.deep.equal(mockResponse);
+          expect(res.body).to.deep.equal(response);
           done();
         });
     });
