cbac84a1ac730ccee02afbbd6858823601f0c8f5
hafizulf <hafizulf68@gmail.com>
2023-02-21 16:11:20 +0700

    refactor: findAll books

--- src/use-cases/book/index.js
+++ src/use-cases/book/index.js
@@ -7,6 +7,7 @@ const buildError = require('../../frameworks/utils/buildError');
 const bookValidator = require('../../frameworks/validators/book');
 
 const fileRemover = require('../../frameworks/utils/fileRemover');
+const paginate = require('../../frameworks/utils/paginate');
 
 // register services
 const serviceFindAll = require('./findAll');
@@ -16,7 +17,8 @@ const serviceDeleteOne = require('./deleteOne');
 const serviceUpdate = require('./update');
 const serviceCountAll = require('./countAll');
 
-const findAll = serviceFindAll(bookRepository);
+const countAll = serviceCountAll(bookRepository);
+const findAll = serviceFindAll(bookRepository, countAll, paginate);
 const save = serviceSave(Entity, bookRepository, bookValidator, buildError);
 const findOne = serviceFindOne(bookRepository);
 const deleteOne = serviceDeleteOne(bookRepository);
@@ -27,7 +29,6 @@ const update = serviceUpdate(
   buildError,
   fileRemover
 );
-const countAll = serviceCountAll(bookRepository);
 
 const bookService = {
   findAll,
