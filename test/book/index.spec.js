2ae2eda106a7efc4c93083448e27087f1ef76e63
hafizulf <hafizulf68@gmail.com>
2023-02-22 14:50:01 +0700

    refactor(books): grouping test specs

---
+++ test/book/index.spec.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+const request = require('supertest');
+
+const app = require('../../src/frameworks/webserver/app');
+const database = require('../../src/frameworks/database/knex');
+const cache = require('../../src/frameworks/middleware/cache');
+
+const mockData = require('../__mock__/book/data');
+const mockResponse = require('../__mock__/book/response');
+
+const findAllSpecs = require('./api.getFindAll.spec');
+const findOneSpecs = require('./api.getFindOne.spec');
+const saveSpecs = require('./api.postSave.spec');
+const deleteSpecs = require('./api.deleteOne.spec');
+const updateSpecs = require('./api.update.spec');
+
+findAllSpecs(expect, request, app, database, cache, mockData, mockResponse);
+findOneSpecs(expect, request, app, database, mockData, mockResponse);
+saveSpecs(expect, request, app, mockResponse);
+deleteSpecs(expect, request, app, database, mockData, mockResponse);
+updateSpecs(expect, request, app, database, mockData, mockResponse);
