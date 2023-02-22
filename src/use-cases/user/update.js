a52ab2cb0e17345f1425b1c1dbde1bc4bceab23e
hafizulf <hafizulf68@gmail.com>
2023-02-22 12:18:37 +0700

    refactor: update user

--- src/use-cases/user/update.js
+++ src/use-cases/user/update.js
@@ -1,6 +1,26 @@
 const serviceUpdate = (Entity, repository, validator, buildError) => {
   return async (data) => {
     try {
+      const { user_id } = data;
+      const userData = await repository.findOne({
+        name: 'user_id',
+        value: user_id,
+      });
+
+      if (!userData) {
+        return {
+          status: false,
+          message: 'User Not Found',
+        };
+      }
+
+      if (Object.keys(data).length === 1) {
+        return {
+          status: false,
+          message: 'Need a field to update',
+        };
+      }
+
       validator.update(data);
 
       const user = new Entity(data);
@@ -10,6 +30,7 @@ const serviceUpdate = (Entity, repository, validator, buildError) => {
 
       return {
         status: true,
+        message: 'User has been updated',
       };
     } catch (err) {
       const errors = buildError(err.inner);
