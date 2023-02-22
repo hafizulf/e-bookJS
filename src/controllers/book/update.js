3975c204c740936c95aedd672e2104b907b70802
hafizulf <hafizulf68@gmail.com>
2023-02-22 13:21:38 +0700

    refactor: books update

--- src/controllers/book/update.js
+++ src/controllers/book/update.js
@@ -1,46 +1,29 @@
 const ctlUpdate = (service) => {
   return async (req, res, next) => {
     const { book_id } = req.params;
-    const book = await service.findOne({ name: 'book_id', value: book_id });
+    const data = req.body;
 
-    if (!book) {
-      return res.status(400).json({
-        status: 'BAD_REQUEST',
-        code: 400,
-        message: 'Book Not Found',
+    data.book_id = book_id;
+    if (req.file !== undefined) {
+      data.file = req.file;
+    }
+
+    const { status, message, errors } = await service.update(data);
+
+    if (status) {
+      return res.status(200).json({
+        status: 'OK',
+        code: 200,
+        message,
       });
     } else {
-      const data = req.body;
-      data.book_id = book_id;
+      const response = {
+        status: 'BAD_REQUEST',
+        code: 400,
+      };
+      message ? (response.message = message) : (response.errors = errors);
 
-      if (req.file !== undefined) {
-        data.file = req.file;
-      }
-
-      if (Object.keys(data).length === 1) {
-        return res.status(400).json({
-          status: 'BAD_REQUEST',
-          code: 400,
-          message: 'Need a field to update',
-        });
-      }
-
-      const oldFile = book.file;
-      const result = await service.update(data, oldFile);
-
-      if (result.status) {
-        return res.status(200).json({
-          status: 'OK',
-          code: 200,
-          message: 'Book has been updated',
-        });
-      } else {
-        return res.status(400).json({
-          status: 'BAD_REQUEST',
-          code: 400,
-          errors: result.errors,
-        });
-      }
+      return res.status(400).json(response);
     }
   };
 };
