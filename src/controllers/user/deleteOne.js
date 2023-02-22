e830cea7cb399bef8751760a41889e9c21bb9aef
hafizulf <hafizulf68@gmail.com>
2023-02-22 12:05:32 +0700

    refactor: delete user

--- src/controllers/user/deleteOne.js
+++ src/controllers/user/deleteOne.js
@@ -1,21 +1,19 @@
 const ctlDeleteOne = (service) => {
   return async (req, res) => {
     const { user_id } = req.params;
-    const result = await service.findOne({ name: 'user_id', value: user_id });
+    const { status, message } = await service.deleteOne(user_id);
 
-    if (!result) {
-      return res.status(400).json({
-        status: 'BAD_REQUEST',
-        code: 400,
-        message: 'User Not Found',
-      });
-    } else {
-      await service.deleteOne(user_id);
-
+    if (status) {
       return res.status(200).json({
         status: 'OK',
         code: 200,
-        message: 'User has been deleted',
+        message,
+      });
+    } else {
+      return res.status(400).json({
+        status: 'BAD_REQUEST',
+        code: 400,
+        message,
       });
     }
   };
--- src/use-cases/user/deleteOne.js
+++ src/use-cases/user/deleteOne.js
@@ -1,6 +1,19 @@
 const serviceDeleteOne = (repository) => {
   return async (user_id) => {
-    await repository.deleteOne(user_id);
+    const user = await repository.findOne({ name: 'user_id', value: user_id });
+
+    if (!user) {
+      return {
+        status: false,
+        message: 'User Not Found',
+      };
+    } else {
+      await repository.deleteOne(user_id);
+      return {
+        status: true,
+        message: 'User has been deleted',
+      };
+    }
   };
 };
 module.exports = serviceDeleteOne;
