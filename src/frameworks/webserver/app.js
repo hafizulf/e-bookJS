a1cdce5b682eee1938580c243d1b47f83e8f86b0
hafizulf <hafizulf68@gmail.com>
2023-03-11 11:21:26 +0700

    add CORS origin

--- src/frameworks/webserver/app.js
+++ src/frameworks/webserver/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const authRoutes = require('../routes/auth.routes');
 const bookRoutes = require('../routes/book.routes');
@@ -9,6 +10,7 @@ const userAccessRoutes = require('../routes/userAccess.routes');
 const app = express();
 
 // plugins
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
