711dcf9be9c60a701f03e945cb5a3a48eb2f3215
hafizulf <hafizulf68@gmail.com>
2023-03-18 23:53:55 +0700

    chore: add console logger, add helmet pkg

--- src/frameworks/webserver/app.js
+++ src/frameworks/webserver/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const fs = require('fs');
 const path = require('path');
@@ -26,10 +28,14 @@ const limit = rateLimit({
   legacyHeaders: false, // Disable the 'X-RateLimit-*' headers
 });
 
+app.use(helmet());
 app.use(cors());
 app.use(limit);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan('combined', { skip: (req, res) => process.env.NODE_ENV === 'test' })
+);
 
 // routes
 app.use('/api/v1/auth', authRoutes);
